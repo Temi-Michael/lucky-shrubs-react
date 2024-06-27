5dd9b5fec4936c3564a7ae3c7fc0da7a851477cc
Temi Michael <olasehindemichael285@gmail.com>
2024-06-27 13:23:51 +0100

    Deployment Push

--- src/views/Services.js
+++ src/views/Services.js
@@ -1,77 +1,79 @@
 import { useState } from "react";
 
 export default function Services() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [info, setInfo] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [info, setInfo] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (info.length < 15) {
-        alert(`Please Specify Clearly The Need.`)
-    } else {
-        alert(`Form Submitted`)
-        setName('')
-        setEmail('')
-        setPhone('')
-        setInfo('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (info.length < 15) {
+            alert(`Please Specify Clearly The Need.`)
+        } else {
+            alert(`Form Submitted`)
+            setName('')
+            setEmail('')
+            setPhone('')
+            setInfo('')
+        }
     }
-  }
 
-  return (
-    <main>
-      <div>
-        <form>
-          <div className='fieldset'>
-            <label htmlFor="name" className="label">
-              Name
-            </label>
-            <input
-              type="text"
-              value={name}
-              id="name"
-              className="input"
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className='fieldset'>
-            <label htmlFor="email" className="label">
-              Email
-            </label>
-            <input
-              type="email"
-              value={email}
-              id="email"
-              className="input"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className='fieldset'>
-            <label htmlFor="phone" className="label">
-              Phone
-            </label>
-            <input
-              type="tel"
-              value={phone}
-              id="phone"
-              className="input"
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div>
-          <div className='fieldset'>
-            <label htmlFor="info" className="label">
-              Info
-            </label>
-            <textarea
-              value={info}
-              className="info"
-              onChange={(e) => setInfo(e.target.value)}
-            />
-          </div>
-          <button className="btn" onClick={handleSubmit}>Submit</button>
-        </form>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div>
+                <form>
+                    <div className='fieldset'>
+                        <label htmlFor="name" className="label">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            value={name}
+                            id="name"
+                            className="input"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className='fieldset'>
+                        <label htmlFor="email" className="label">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            value={email}
+                            id="email"
+                            className="input"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className='fieldset'>
+                        <label htmlFor="phone" className="label">
+                            Phone
+                        </label>
+                        <input
+                            type="tel"
+                            value={phone}
+                            id="phone"
+                            className="input"
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </div>
+                    <div className='fieldset'>
+                        <label htmlFor="info" className="label">
+                            Info
+                        </label>
+                        <textarea
+                            value={info}
+                            className="info"
+                            onChange={(e) => setInfo(e.target.value)}
+                        />
+                    </div>
+                    <div className="btn-frame">
+                        <button className="btn" onClick={handleSubmit}>Submit</button>
+                    </div>
+                </form>
+            </div>
+        </main>
+    );
 }
